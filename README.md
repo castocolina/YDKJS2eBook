01844e4b2c5fee0b5b66ae1c9234754fc3b173c9
Casto Colina <castocolina@gmail.com>
2018-11-23 22:30:36 -0300

    Order pages

--- README.md
+++ README.md
@@ -10,13 +10,10 @@ brew cask install calibre
 ```
 
 ```
-go run to_epub.go
+go run toebook.go
 ```
-
--- kindlegen css-refresher-notes.epub
 
 ## References
 
 - https://pandoc.org/MANUAL.html
 - https://gist.github.com/bmaupin/6e3649af73120fac2b6907169632be2c
-- https://github.com/pbakondy/til/blob/master/markdown/markdown-to-ebook.md
--- toebook.go
+++ toebook.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strings"
 )
 
@@ -16,18 +17,16 @@ type MkFile struct {
 	FileName     string
 }
 
+const BookAuthor = "Kyle Simpson"
+const BookDir = "You-Dont-Know-JS"
+const BookRepoURL = "https://github.com/getify/You-Dont-Know-JS.git"
+const BookEpubCSSURL = "https://gist.githubusercontent.com/bmaupin/6e3649af73120fac2b6907169632be2c/raw/epub.css"
+const MetaTittlePrefix = "You Don't Know JS :"
+
+const Page1Forework = "foreword.md"
+const Page2PrefacePage = "../preface.md"
+
 func main() {
-
-	const BookAuthor = "Kyle Simpson"
-	const BookDir = "You-Dont-Know-JS"
-	const BookRepoURL = "https://github.com/getify/You-Dont-Know-JS.git"
-	const BookEpubCSSURL = "https://gist.githubusercontent.com/bmaupin/6e3649af73120fac2b6907169632be2c/raw/epub.css"
-	const MetaTittlePrefix = "You Don't Know JS: "
-
-	//-f --from
-	const InputFormart = "gfm" //markdown_github (deprecated) | gfm  (GitHub-Flavored Markdown)
-	//-t --to
-	const OutputFormat = "epub" // kindle (mobi, awz...) not supported
 
 	var books = map[string]string{
 		"up & going":               MetaTittlePrefix + " 1 - Up & Going",
@@ -51,16 +50,18 @@ func main() {
 		execExternal(workDir, "git", "-C", BookDir, "pull", "--progress", "-v")
 	}
 	execExternal(workDir, "wget", "-O", BookDir+"/epub.css", BookEpubCSSURL)
+	replaceFileInline(BookDir + "/preface.md")
 
 	for folder, bookName := range books {
 		var bookFolder = BookDir + "/" + folder
 
+		execExternal(bookFolder, "rm", "-rf", "README.md", "toc.md")
 		log.Println(bookName)
 		var mkFiles = checkExt(bookFolder, ".md")
-		var sigleMkNames []string
+		var singleMkNames []string
 		for _, mkFile := range mkFiles {
 			replaceFileInline(mkFile.FullFileName)
-			sigleMkNames = append(sigleMkNames, mkFile.FileName)
+			singleMkNames = append(singleMkNames, mkFile.FileName)
 		}
 
 		epubOptions := []string{
@@ -69,16 +70,65 @@ func main() {
 			// "--no-highlight",
 			"-M", fmt.Sprintf("author=\"%s\"", BookAuthor),
 			"-M", fmt.Sprintf("title=\"%s\"", bookName),
-			"-M", "lang=en-US",
-			"--verbose", "--log=log.json", "../preface.md"}
-		epubOptions = append(epubOptions, sigleMkNames...)
+			"-M", "lang=en-US", "--log=log.json",
+			// "--verbose",
+		}
+		singleMkNames = append(singleMkNames, Page2PrefacePage)
+		singleMkNames = orderPages(singleMkNames)
+		epubOptions = append(epubOptions, singleMkNames...)
 
 		fmt.Println(epubOptions)
 		buildEpub(bookFolder, epubOptions...)
 		execExternal(BookDir, "ebook-convert", fmt.Sprintf("%s.epub", bookName), fmt.Sprintf("%s.mobi", bookName))
 	}
 
+	fmt.Println()
 	execExternal(BookDir, "ls")
+}
+
+type byLessPages []string
+
+func (s byLessPages) Len() int {
+	return len(s)
+}
+func (s byLessPages) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byLessPages) Less(i, j int) bool {
+	//apA.md ch1.md ch2.md ch3.md toc.md foreword.md ../preface.md
+	// TOBE == foreword.md  ../preface.md ch1.md ch2.md ch3.md apA.md
+	var pageA, pageB = s[i], s[j]
+	var less bool
+	//ugly way
+	if pageA == Page1Forework {
+		less = true
+	} else if pageB == Page1Forework {
+		less = false
+	} else if pageA == Page2PrefacePage {
+		less = true
+	} else if pageB == Page2PrefacePage {
+		less = false
+	} else if strings.HasPrefix(pageA, "ch") && !strings.HasPrefix(pageB, "ch") {
+		//fmt.Printf("COMPARE ch !ch >> %s -- %s \n", pageA, pageB)
+		less = true
+	} else if !strings.HasPrefix(pageA, "ch") && strings.HasPrefix(pageB, "ch") {
+		//fmt.Printf("COMPARE !ch ch >> %s -- %s \n", pageA, pageB)
+		less = false
+	} else {
+		//fmt.Printf("COMPARE %s -- %s \n", pageA, pageB)
+		less = pageA < pageB
+	}
+	return less
+}
+
+func orderPages(files []string) []string {
+	// const pages := [len(files)] files
+	fmt.Println()
+	sort.Sort(byLessPages(files))
+	fmt.Println()
+
+	return files
 }
 
 func replaceFileInline(fileName string) {
@@ -97,14 +147,14 @@ func replaceFileInline(fileName string) {
 	for i, line := range lines {
 		r, err := regexp.MatchString(DeleteYouDontKnow, line)
 		if err == nil && r {
-			fmt.Printf("Delete ['%s'] in file >> %s\n", line, fileName)
+			// fmt.Printf("Delete ['%s'] in file >> %s\n", line, fileName)
 			line = ""
 		}
 		line = strings.Replace(line, BrFind, BrRepleace, -1)
 
 		if ReImg.MatchString(line) {
 			var imgLine = ReImg.ReplaceAllString(line, SubsImg)
-			fmt.Printf("Replace IMG ['%s'] with >> %s\n", line, imgLine)
+			// fmt.Printf("Replace IMG ['%s'] with >> %s\n", line, imgLine)
 			line = imgLine
 		}
 
@@ -139,7 +189,8 @@ func execExternal(folder string, command string, options ...string) []byte {
 	if err != nil {
 		log.Fatalf("\n\t cmd.Run() failed with %s\n%s\n ==> %s -- %s", err, out, command, options)
 	}
-	fmt.Printf("\n combined out:\n%s\n", string(out))
+	//fmt.Printf("\n combined out:\n%s\n", string(out))
+	fmt.Println(string(out))
 	return out
 }
 
@@ -150,5 +201,6 @@ func buildEpub(folder string, options ...string) {
 	if err != nil {
 		log.Fatalf("\n\t cmd.Run(pandoc) failed with %s\n%s", err, out)
 	}
-	fmt.Printf("\n combined out:\n%s\n", string(out))
+	// fmt.Printf("\n combined out:\n%s\n", string(out))
+	fmt.Println(string(out))
 }
